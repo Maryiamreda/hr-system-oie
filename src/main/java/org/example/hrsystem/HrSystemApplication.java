c0225ecd576ebcf9959dd3b437618a6655cf245e
Maryiamreda <maryiamreda@gmail.com>
2025-12-29 11:20:39 +0200

    initial commit

---
+++ src/main/java/org/example/hrsystem/HrSystemApplication.java
@@ -0,0 +1,12 @@
+package org.example.hrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrSystemApplication.class, args);
+        System.out.println("السلام عليكم");
+    }
+}
