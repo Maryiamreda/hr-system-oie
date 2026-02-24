20cb5dd7cd79bd1d0358054dab80fec3a76b5fd4
Maryiamreda <maryiamreda@gmail.com>
2026-02-24 11:30:22 +0200

    add semantic-release setup

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: ["master"],  //the main branch only will trigger releases
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    "@semantic-release/git"
+  ]
+};
